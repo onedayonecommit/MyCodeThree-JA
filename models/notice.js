6bb27db5ac98730ef3fe9bba67864706cf0beed7
onedayonecommit <rudghks0981@gmail.com>
2022-08-31 22:12:46 +0900

    notice table create

---
+++ models/notice.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+class Notice extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        nno: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        title: {
+          type: Sequelize.STRING(1000),
+          allowNull: false,
+        },
+        content: {
+          type: Sequelize.STRING(5000),
+          allowNull: false,
+        },
+        nickname: {
+          type: Sequelize.STRING(1000),
+          unique: true,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        underscored: true, // 대문자를 언더바로 카멜을 스네이크로
+        timestamps: true,
+        modelName: "Notice",
+        tableName: "notice",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+}
+
+module.exports = Notice;
